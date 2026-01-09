3153db6e67e5a18e947000f3c7192a7973638c3c
P3drio <perodriguez@unal.edu.co>
2026-01-09 16:31:27 -0600

    Engine fixed, py files updated

--- Python/load_to_db.py
+++ Python/load_to_db.py
@@ -11,6 +11,10 @@ PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, ".."))  # project root
 RAW_DATA_PATH = os.path.join(PROJECT_ROOT, "data", "raw", "online_retail_II.csv")
 DB_PATH = os.path.join(PROJECT_ROOT, "data", "retail.db")
 
+# File check
+if not os.path.exists(RAW_DATA_PATH):
+    raise FileNotFoundError(f"Raw data not found: {RAW_DATA_PATH}")
+
 # Load raw dataset
 df = pd.read_csv(RAW_DATA_PATH, encoding='unicode_escape')
 
@@ -20,6 +24,10 @@ df = df.rename(columns={
     'InvoiceDate': 'InvoiceDate',
     'Price': 'UnitPrice'
 })
+
+# Create SQLite engine
+
+engine = create_engine(f"sqlite:///{DB_PATH}")
 
 # Load raw data into database
 df.to_sql(
